371795a9db40706d27db3682324044846945ab35
Kira - San <51356979+DarkFallenAngel@users.noreply.github.com>
2019-06-29 20:31:48 +0700

    Update AngelBeach.sh

--- AngelBeach.sh
+++ AngelBeach.sh
@@ -1,1 +1,16 @@
 
+#!/bin/bash
+#version 1.0
+#variables
+w='\e[97m'
+g='\033[1;92m'
+r='\033[1;91m'
+a='\033[1;94m'
+b='\e[1;4m'
+cyan='\033[1;36m'
+green='\033[1;32m'
+red='\033[1;31m'
+yellow='\033[1;33m'
+blue='\033[1;34m'
+purple='\033[1;35m'
+reset='\033[0m'
